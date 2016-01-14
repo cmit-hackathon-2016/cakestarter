5c2d43e7e3602f6e6a6131dcbfc809b8ea5de54c
Frederik Petersson <Frederik.Petersson@nordea.com>
2016-01-15 00:31:04 +0100

    consolidated css into 1 file instead of 2. Added app.js functionality to update the games status. Added watchers for changes and prepared for playing audio files.
    todo: make different set of divs appear on winning state

--- app/app.js
+++ app/app.js
@@ -2,11 +2,45 @@
 
 var myApp = angular.module('myApp', []);
 
-myApp.controller('CakeController', ['$scope', 'playerService', function($scope, playerService) {
-  $scope.participants = [];
-  $scope.currentTotal = 25 + 12;
+myApp.controller('CakeController', ['$scope', 'playerService', '$interval', function($scope, playerService, $interval) {
+    $scope.participants = [];
+    $scope.currentTotal = 25 + 12;
+    $scope.gameOver=false;
 
-  playerService.findAll().then(function(result) {
-    $scope.participants = result.data;
-  });
+    function updateGameState() {
+        playerService.findAll().then(function(result) {
+            $scope.participants = result.data;
+        });
+
+        gameService.getTotal().then(function(result) {
+            $scope.currentTotal = result.data;
+        });
+
+        gameService.isGameOver().then(function(result){
+           $scope.gameOver = result.data;
+        });
+    }
+
+    $interval(updateGameState, 500);
+
+    $scope.$watchCollection("participants", function(next, prev){
+        if(next == true){
+            var audio = new Audio('audio_file.mp3'); //play some audio when new player joins
+            audio.play();
+        }
+    });
+
+    $scope.$watch("currentTotal", function(next, prev){
+        if(next >= prev){
+            var audio = new Audio('audio_file.mp3'); //play some audio when pool increases (someone pays bitcoins)
+            audio.play();
+        }
+    });
+
+    $scope.$watch("gameOver", function(){
+        if(gameOver === true){
+            var audio = new Audio('audio_file.mp3'); //play some sound when bomb explodes
+            audio.play();
+        }
+    });
 }]);
--- app/services/game-service.js
+++ app/services/game-service.js
@@ -2,6 +2,10 @@ angular.module('myApp').factory('gameService', ['$http', function($http) {
   var service = {
     getTotal: function() {
       return $http.get('/total');
+    },
+
+    isGameOver: function() {
+      return $http.get('/isGameOver');
     }
   };
   return service;
