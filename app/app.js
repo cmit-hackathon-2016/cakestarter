2132037fa60dc88dd50479e99f078867552f20fe
Frederik Petersson <Frederik.Petersson@nordea.com>
2016-01-14 20:35:25 +0100

    added mockdata and controller for the app
    controller + mockdata

--- app/app.js
+++ app/app.js
@@ -1,4 +1,12 @@
 'use strict';
 
-angular.module('myApp', []);
+var myApp = angular.module('myApp', []);
 
+myApp.controller('CakeController', ['$scope', function($scope) {
+
+    $scope.participants = [{"name": "name1"}, {"name": "name2"}];
+
+    $scope.currentTotal = 25 + 12;
+
+    
+}]);
