2c4036615fcc70715df3c263d256a21ca846160c
Julian <julian@contingent.dk>
2016-01-18 22:49:51 +0100

    fix wrong field

--- app/services/game-service.js
+++ app/services/game-service.js
@@ -2,7 +2,7 @@ angular.module('myApp').factory('gameService', ['$http', function($http) {
   var service = {
     status: function() {
       return $http.get('/status').then(function (result) {
-          return result.status;
+          return result.data;
       });
     },
   };
